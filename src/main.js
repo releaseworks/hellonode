5b8e686a766b6375ee68cf438f5381abfb9077bf
romainx <cros.romain@gmail.com>
2019-06-02 15:40:24 +0200

    Modif message

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.end("Hello World!\n");
 });
 
 // listen on localhost:8000
