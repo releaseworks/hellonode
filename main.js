8bb94b521534dc4407f0472c54dc13b6559f116b
MD ABRAR <mdabrar.bvb@gmail.com>
2019-07-08 18:50:13 +0530

    Update main.js
    Added name

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.end("Hello Abrar this is an update getintodevops.com\n");
 });
 
 // listen on localhost:8000
