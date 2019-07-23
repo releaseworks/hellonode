ab5d3f2b1e8e79c24e450022651c261f32cc6607
MD ABRAR <mdabrar.bvb@gmail.com>
2019-07-23 18:00:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // load the http module
-var http = require('http');
+var https = require('https');
 
 // configure our HTTP server
-var server = http.createServer(function (request, response) {
+var server = https.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello Abrar this is an update mdabrar.bvb@gmail.com\n This is a new change at 11:46AM");
 });
 
 // listen on localhost:8000
 server.listen(3000);
-console.log("Server listening at http://127.0.0.1:3000/");
+console.log("Server listening at http://0.0.0.0:3000/");
